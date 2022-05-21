dbf8f41070069e67fd0500f98700d9dd5fc3c60e
xx10222 <yjw4960@gmail.com>
2022-05-21 20:43:11 +0900

    [feature/login] 로그인 추가

--- src/main/java/com/neodinary7/hackathon/User/UserController.java
+++ src/main/java/com/neodinary7/hackathon/User/UserController.java
@@ -1,8 +1,14 @@
 package com.neodinary7.hackathon.User;
 
+import com.neodinary7.hackathon.User.model.LoginRequest;
+import com.neodinary7.hackathon.User.model.LoginResponse;
 import com.neodinary7.hackathon.User.model.UserRequest;
+import com.neodinary7.hackathon.config.BaseException;
+import com.neodinary7.hackathon.config.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import static com.neodinary7.hackathon.config.BaseResponseStatus.*;
 
 @RestController
 @RequestMapping("/users")
@@ -10,9 +16,31 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
+    @ResponseBody
     @PostMapping("/signup") //회원가입
-    public String singup(@RequestBody UserRequest userRequest) {
-        userService.addUser(userRequest);
-        return "success";
+    public BaseResponse<String> singup(@RequestBody UserRequest userRequest) {
+        try {
+            if(userService.existUser(userRequest.getEmail()) != 0) {
+                return new BaseResponse<>(EXIST_USER_ERROR);
+            }
+            userService.addUser(userRequest);
+            String result = "success!";
+            return new BaseResponse<>(result);
+        } catch (BaseException exception) {
+            return new BaseResponse<>(exception.getStatus());
+        }
+    }
+
+    @GetMapping("/login") //로그인
+    public BaseResponse<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        try {
+            if(userService.existUser(loginRequest.getEmail()) == 0) {
+                return new BaseResponse<>(NOT_EXIST_USER);
+            }
+            LoginResponse loginResponse = userService.getLogin(loginRequest);
+            return new BaseResponse<>(loginResponse);
+        } catch (BaseException exception) {
+            return new BaseResponse<>(exception.getStatus());
+        }
     }
 }
--- src/main/java/com/neodinary7/hackathon/User/UserDao.java
+++ src/main/java/com/neodinary7/hackathon/User/UserDao.java
@@ -1,5 +1,8 @@
 package com.neodinary7.hackathon.User;
 
+import com.neodinary7.hackathon.User.model.LoginRequest;
+import com.neodinary7.hackathon.User.model.LoginResponse;
+import com.neodinary7.hackathon.User.model.User;
 import com.neodinary7.hackathon.User.model.UserRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,10 +16,25 @@ public class UserDao {
     private final JdbcTemplate jdbcTemplate;
 
     public void addUser(UserRequest userRequest) {
-        log.info("userRequest : {}", userRequest);
-
         String Query = "insert into User(name, email, password) values (?,?,?);";
         Object[] userData = new Object[]{userRequest.getName(), userRequest.getEmail(), userRequest.getPassword()};
         this.jdbcTemplate.update(Query, userData);
+    }
+
+    public int existUser(String email) {
+        String Query = "select exists(select userIdx from User where email = ?) as success;";
+        int id = 0;
+        id = this.jdbcTemplate.queryForObject(Query, Integer.class, email);
+        return id;
+    }
+
+    public User getUser(String email) {
+        String Query = "select * from User where email = ?";
+        return this.jdbcTemplate.queryForObject(Query,
+                (rs,rowNum) -> new User(
+                        rs.getInt("userIdx"),
+                        rs.getString("name"),
+                        rs.getString("email"),
+                        rs.getString("password")), email);
     }
 }
