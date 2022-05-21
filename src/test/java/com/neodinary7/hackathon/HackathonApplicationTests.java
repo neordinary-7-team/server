b546fb271709e5f2c11d08e3e185003e8c352716
xx10222 <yjw4960@gmail.com>
2022-05-21 14:20:04 +0900

    first commit : initail project structure

---
+++ src/test/java/com/neodinary7/hackathon/HackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neodinary7.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
